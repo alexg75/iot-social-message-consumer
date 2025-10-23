835d4d6a89565f4e09bef72fb4e0b574d18dbdd6
alexgrimaldi <alexgrimaldi@gmail.com>
2025-10-23 10:43:44 +0100

    fix issue with slackClient

--- slackClient.py
+++ slackClient.py
@@ -1,8 +1,9 @@
-# from slack_sdk import WebClient
-# from slack_sdk.errors import SlackApiError
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 import logger
 from socialMessageUtils import removeSlackChannel
 import tokenUtils
+import json
 
 log = logger.setup_logger("slackClient")
 SLACK_CHANNEL_KEY = "slack_channel"
@@ -12,16 +13,21 @@ def send(message):
     
     channel_name = message[SLACK_CHANNEL_KEY]; 
     message = removeSlackChannel(message); 
-    access_token = tokenUtils.load_stored_token()
+    token = tokenUtils.load_stored_token()
+    access_token = token["access_token"]
+    messageAsString = json.dumps(message)
 
-    log.info(f"channel_name -->{channel_name}<--\n\r")
-    log.info(f"message -->{message}<--\n\r")
-    log.info(f"access_token -->{access_token}<--\n\r")
-    # client = WebClient(token=access_token)
-    # try:
-    #     client.chat_postMessage(
-    #         channel=channel_name,
-    #         text=message
-    #     )
-    # except SlackApiError as e:
-    #     log.error(f"Error: {e}")    
+    log.debug(f"channel_name -->{channel_name}<--\n\r")
+    log.debug(f"message -->{message}<--\n\r")
+    log.debug(f"messageAsString -->{messageAsString}<--\n\r")
+    log.debug(f"token -->{token}<--\n\r")
+    log.debug(f"access_token -->{access_token}<--\n\r")
+    
+    client = WebClient(token=access_token)
+    try:
+        client.chat_postMessage(
+            channel=channel_name,
+            text=messageAsString
+        )
+    except SlackApiError as e:
+        log.error(f"Error: {e}")    
